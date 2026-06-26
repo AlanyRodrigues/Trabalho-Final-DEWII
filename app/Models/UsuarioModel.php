874cce226c9e6473b4610fb6f6488e1291d55606
Deivid Henrique Nunes da Costa <87528137+DeividHNC@users.noreply.github.com>
2026-06-26 10:16:23 -0300

    UsuarioModel

--- app/Models/UsuarioModel.php
+++ app/Models/UsuarioModel.php
@@ -4,23 +4,42 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class ClientesModel extends Model
+class UsuarioModel extends Model
 {
-    protected $table = 'clientes';
-    protected $primaryKey = 'codigo';
-    protected $useAutoIncrement = true;
-    protected $returnType = 'array';
-    
-    protected $allowedFields = ['nome', 'cpf', 'estado_id', 'cidade_id'];
+    protected $table = 'usuarios';
+    protected $primaryKey = 'id';
 
-    public function getClientesComLocalizacao()
-{
-    return $this->select('clientes.*, municipios.nome as cidade_nome, estados.nome as estado_nome')
-                ->join('municipios', 'municipios.id = clientes.cidade_id', 'left')
-                ->join('estados', 'estados.id = clientes.estado_id', 'left')
-                ->orderBy('clientes.nome', 'ASC')
-                ->findAll();
-}
+    protected $useAutoIncrement = true;
+    
+    protected $returnType = 'array';
+    protected $allowedFields = ['nome', 'email'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 }
 
 ?> 
