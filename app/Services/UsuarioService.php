ee4850fa66419ff67bd3b02431b54dfa767d9191
Deivid Henrique Nunes da Costa <87528137+DeividHNC@users.noreply.github.com>
2026-06-26 10:45:41 -0300

    UsuarioService - funções get, cadastrar, login

--- app/Services/UsuarioService.php
+++ app/Services/UsuarioService.php
@@ -2,26 +2,81 @@
 
 namespace App\Services;
 
-use App\Models\EstadoModel;
+use App\Models\UsuarioModel;
 
-class EstadoService 
-{
-    public function getEstados() 
-    {
+class UsuarioService {
+
+    public function getUsuarios() {
         try {
-            $estadoModel = new EstadoModel();
-            
-            $estados = $estadoModel->getEstados(); 
+            $usuarioModel  = new UsuarioModel();
+            $usuarios = $usuarioModel->findAll(); 
 
             return [
                 'status' => 'success',
-                'data'   => $estados
+                'data'   => $usuarios
             ];
 
         } catch (\Exception $e) {
             return [
                 'status'  => 'error',
                 'message' => "Erro no banco de dados: " . $e->getMessage()
+            ];
+        }
+    }
+
+    public function cadastrarUsuario(array $dados) {
+        try {
+            $usuarioModel  = new UsuarioModel();
+
+            $dados['senha'] = $dados['senha'];
+            $dados['token_recuperacao'] = '';
+
+            $usuarioModel->insert($dados);
+
+            return [
+                'status' => 'success',
+                'message' => 'Usuário cadastrado com sucesso.'
+            ];
+
+        } catch (\Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+
+    public function loginUsuario($email, $senha) {
+        try {
+            $usuarioModel  = new UsuarioModel();
+
+            $usuario = $usuarioModel
+                ->where('email', $email)
+                ->first();
+
+            if (!$usuario) {
+                return [
+                    'status' => 'error',
+                    'message' => 'Usuário não encontrado.'
+                ];
+            }
+
+            if ($usuario['senha'] !== $senha) {
+                return [
+                    'status' => 'error',
+                    'message' => 'Senha incorreta.'
+                ];
+            }
+
+            return [
+                'status' => 'success',
+                'data' => $usuario
+            ];
+
+        } catch (\Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => $e->getMessage()
             ];
         }
     }
